348b07504ae0a75469a9244f0674a1bba0c2ee9e
Sergio Fernández Avila <fefsd@gmailcom>
2024-10-30 17:30:15 +0000

    Primer Commit

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/biblio/";
+const base_url = "http://10.2.4.111/biblioteca/";
 const host = "localhost";
-const user = "root";
-const pass = "";
+const user = "biblioteca";
+const pass = "usuario";
 const db = "biblioteca";
 const charset = "charset=utf8";
 ?>
